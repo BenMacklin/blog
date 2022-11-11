ddfccec0bed8e80de9477208ba40bec56904b190
BenMacklin <benjaminmacklin92@gmail.com>
2022-11-10 21:03:23 -0500

    Comment destroy dependant on Articles and date formated

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,24 +1,18 @@
 class Article < ApplicationRecord
-
-  has_many :comments
+  include Visible
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: {in: VALID_STATUSES}
 
   def change
     create_table :articles do |t|
       t.string :title
       t.string :body
 
-      t.timestamps
-    end
-  end
+      t.timestamps.time_zone
+      t.string :status
 
-  def archived?
-    status == 'archived'
+    end
   end
 end
